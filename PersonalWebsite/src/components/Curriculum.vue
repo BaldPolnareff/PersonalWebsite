8c317e2b09b9fe470e1d7b0d3522639c6af884ac
BaldPolnareff <baldpolnareff@gmail.com>
2024-03-28 14:15:14 +0100

    added a simple collapse for the CV component

---
+++ PersonalWebsite/src/components/Curriculum.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const activeKey = ref<string[]>(['1', '2', '3', '4']);
+</script>
+
+<template>
+    <div class="cv-container">
+        <a-collapse 
+            class="collapse" 
+            v-model:activeKey="activeKey"
+            :bordered="false"
+            ghost
+        >
+            <a-collapse-panel key="1" header="Work Experience">
+                <p>Work Experience</p>
+            </a-collapse-panel>
+            <a-collapse-panel key="2" header="Education">
+                <p>Education</p>
+            </a-collapse-panel>
+            <a-collapse-panel key="3" header="Publications">
+                <p>Publications</p>
+            </a-collapse-panel>
+            <a-collapse-panel key="4" header="Computer Skills">
+                <p>Computer Skills</p>
+            </a-collapse-panel>
+        </a-collapse>
+    </div>
+</template>
+
+<style scoped>
+.cv-container {
+    margin-top: 10px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 150px;
+}
+
+.collapse {
+    background-color: rgb(26, 26, 26);
+    color: aliceblue !important;
+}
+
+</style>
