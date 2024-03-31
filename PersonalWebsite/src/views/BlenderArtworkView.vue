ec5da4f0f0c7d64e935c67a2f9b9f17ba4b1e5f7
BaldPolnareff <baldpolnareff@gmail.com>
2024-03-31 17:03:51 +0200

    image fetching logic implemented

--- PersonalWebsite/src/views/BlenderArtworkView.vue
+++ PersonalWebsite/src/views/BlenderArtworkView.vue
@@ -1,10 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { supabase } from '@/utils/supabase';
+
+const imageUrls = ref<string[]>([]);
+const errorMessage = ref<string>('');
+const loadingImages = ref<boolean>(true);
+const loadingImage = ref<boolean>(false);
+
+async function fetchImages(): Promise<void> {
+    const imagesNames: string[] = [];
+
+    loadingImages.value = true;
+    const { data: imagePaths, error } = await supabase
+        .storage
+        .from('blender_gallery')
+        .list('jpeg');
+
+    if (error) {
+        loadingImages.value = false;
+        errorMessage.value = error.message;
+    } else {
+        for (let i = 0; i < imagePaths.length; i++) {
+            imagesNames.push(imagePaths[i].name);
+        }
+
+        for (let i = 0; i < imagesNames.length; i++) {
+            loadingImage.value = true;
+            const { data: publicUrl } = await supabase 
+                .storage
+                .from('blender_gallery')
+                .getPublicUrl(imagesNames[i]);
+
+            if (publicUrl) {
+                imageUrls.value.push(publicUrl.publicUrl);
+                loadingImage.value = false;
+            }
+            else {
+                errorMessage.value = `Error getting public url for ${imagesNames[i]}`;
+                loadingImage.value = false;
+            }
+        }
+    }
+    errorMessage.value = '';
+    loadingImages.value = false;
+    loadingImage.value = false;
+}
+
+onMounted( async () => {
+    await fetchImages()
+})
+</script>
 
 <template>
     <main>
         <div class="gallery-container">
             <h1>Blender Artwork</h1>
-            <p>Here goes the gallery</p>
+            <p>Here's some of the artwork I created in Blender, you can get them in full res.</p>
         </div>
     </main>
 </template>
