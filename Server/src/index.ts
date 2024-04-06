6d59621735001f34ff30487eb95a91d46deb71e2
BaldPolnareff <baldpolnareff@gmail.com>
2024-04-06 18:38:32 +0200

    changed local port of server to 8080

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const init = async () => {
     const server = Hapi.server({
-        port: 3000, 
+        port: 8080, 
         host: 'localhost', 
         routes: {
             cors: {
