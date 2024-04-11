984d4741ab134a36d863ae8ee9d901ab6d0be483
BaldPolnareff <baldpolnareff@gmail.com>
2024-04-11 19:29:32 +0200

    successfully implemented alternative to v-lazy-image, it's not as good but almost equivalent, let's hope it causes no trouble in deployment

--- PersonalWebsite/src/main.ts
+++ PersonalWebsite/src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'ant-design-vue/dist/reset.css';
 import antd from 'ant-design-vue';
+import VueLazyload from 'vue3-lazyload';
 
 import App from './App.vue'
 import router from './router'
@@ -12,6 +13,21 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(antd)
+app.use(VueLazyload, {
+    loading: '',
+    error: '',
+    lifecycle: {
+        loading: (el) => {
+        console.log('loading', el)
+        },
+        error: (el) => {
+        console.log('error', el)
+        },
+        loaded: (el) => {
+        console.log('loaded', el)
+        }
+    }
+})
 app.use(router)
 
 app.mount('#app')
--- PersonalWebsite/vite.config.ts
+++ PersonalWebsite/vite.config.ts
@@ -17,10 +17,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }, 
-  build: {
-    rollupOptions: {
-      external: 'v-lazy-image'
-    }
-  },
-  base: '/PersonalWebsite'
+  // base: '/PersonalWebsite'
 })
